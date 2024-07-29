27fa847d8fbc481f18900728ad6bfed78af23fbf
ABHIRUP <abhirupsaha02@gmail.com>
2024-07-30 05:09:00 +0530

    .

--- socket.js
+++ socket.js
@@ -3,6 +3,7 @@
 import { io } from 'socket.io-client'
 
 export const socket = io(process.env.NEXT_PUBLIC_API_URL, {
+    transports: ['websocket'],
     extraHeaders: {
         'Access-Control-Allow-Origin': '*'
     }
