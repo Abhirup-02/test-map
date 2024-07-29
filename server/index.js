e49bc4e9c581215b379ac7fe628d6595163a8991
ABHIRUP <abhirupsaha02@gmail.com>
2024-07-29 22:48:28 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -2,25 +2,24 @@ import express from "express"
 import { Server } from 'socket.io'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import { createServer } from 'node:http'
 
 // process.loadEnvFile()
 dotenv.config()
 
 const app = express()
+const httpServer = createServer(app)
 
 app.use(cors({ origin: '*' }))
 app.use(express.json())
 
+
 app.get('/', (req, res) => res.json('Map API wishes you good day'))
 
-const port = process.env.PORT || 3000
-const server = app.listen(port, () => {
-    console.log(`Server PORT -> ${port}`)
-})
 
-const io = new Server(server, {
-    pingTimeout: 60000,
-    cors: '*'
+const io = new Server(httpServer, {
+    cors: process.env.FRONTEND_URL,
+    pingTimeout: 60000
 })
 
 
@@ -29,7 +28,7 @@ io.on('connection', (socket) => {
 
     // user should join a room in our case i think the room should be walletID
     socket.on('share-location', (location) => {
-        // console.log(location);
+        console.log(location);
 
         socket.broadcast.emit('share-location', location)
     })
@@ -39,4 +38,9 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         console.log('User Disconnected.')
     })
+})
+
+const port = process.env.PORT || 3000
+httpServer.listen(port, () => {
+    console.log(`Server PORT -> ${port}`)
 })
